0c5976774b38adafba8745cd60a379bf4ae6ad11
Meet Bhanushali <iitjeemeet@gmail.com>
2023-05-20 23:25:22 +0530

    add css

--- src/web/app.py
+++ src/web/app.py
@@ -1,5 +1,6 @@
 import requests
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, render_template, request, redirect, url_for
+import random
 
 app = Flask(__name__)
 
@@ -55,6 +56,31 @@ def get_movie_embed_link(imdb_id):
     embed_link = f'https://vidsrc.me/embed/{imdb_id}/'
     return embed_link
 
+def get_random_movies():
+    response = requests.get('https://imdb-api.com/en/API/MostPopularMovies/k_c7k913z5')
+    data = response.json()
+
+    movies = []
+    if 'items' in data:
+        random.shuffle(data['items'])
+        for item in data['items'][:6]:
+            movies.append({
+                'title': item['title'],
+                'year': item['year'],
+                'imdb': item['id'],
+                'poster': item['image'],
+                'genre': '',
+                'runtime': '',
+                'director': '',
+                'country': '',
+                'rating': '',
+                'votes': '',
+                'sub': '',
+                'quality': ''
+            })
+
+    return movies
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     latest_movies = get_latest_movies()
@@ -77,7 +103,8 @@ def search():
 @app.route('/movie/<imdb_id>')
 def movie(imdb_id):
     embed_link = get_movie_embed_link(imdb_id)
-    return render_template('movie.html', embed_link=embed_link)
+    related_movies = get_random_movies()
+    return render_template('movie.html', embed_link=embed_link, related_movies=related_movies)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
