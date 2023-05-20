293676c94724c0bd3bbd7a8e9e5e123535b40b04
Meet Bhanushali <iitjeemeet@gmail.com>
2023-05-20 22:51:16 +0530

    add the 0.0.0.0

--- src/web/app.py
+++ src/web/app.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for 
 
 app = Flask(__name__)
 
