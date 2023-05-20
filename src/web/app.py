da6127a1792a4186a56292f4c16a3354fd856af9
Meet Bhanushali <iitjeemeet@gmail.com>
2023-05-21 03:34:23 +0530

    sorry

--- src/web/app.py
+++ src/web/app.py
@@ -4,8 +4,6 @@ import random
 
 app = Flask(__name__)
 
-IMDB_API_KEY = 'k_5pjllrms'
-
 def get_latest_movies():
     response = requests.get('https://api.gdriveplayer.us/v1/movie/newest', params={'limit': 10, 'page': 1, 'order': 'date', 'sort': 'DESC'})
     data = response.json()
@@ -30,7 +28,7 @@ def get_latest_movies():
     return movies
 
 def search_movies(title):
-    response = requests.get(f'https://imdb-api.com/en/API/SearchTitle/{IMDB_API_KEY}/{title}')
+    response = requests.get(f'https://imdb-api.com/en/API/SearchTitle/k_c7k913z5/{title}')
     data = response.json()
 
     movies = []
@@ -55,11 +53,11 @@ def search_movies(title):
     return movies
 
 def get_movie_embed_link(imdb_id):
-    embed_link = f'http://database.gdriveplayer.us/player.php?imdb={imdb_id}'
+    embed_link = f'https://vidsrc.me/embed/{imdb_id}/'
     return embed_link
 
 def get_random_movies():
-    response = requests.get(f'https://imdb-api.com/en/API/MostPopularMovies/{IMDB_API_KEY}')
+    response = requests.get('https://imdb-api.com/en/API/MostPopularMovies/k_c7k913z5')
     data = response.json()
 
     movies = []
